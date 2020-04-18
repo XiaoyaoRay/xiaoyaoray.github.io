1f348300213df1a8d6b23d319d2e82e14b4b8c64
ritboy <ritboylei@qq.com>
2020-04-18 13:21:39 +0800

    Site updated: 2020-04-18 13:21:39

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			cdnPath: "https://gitee.com/sswhite/live2d_api/blob/master/"   //存放在gitee上 可以fork到自己仓库
+			cdnPath: "ttp://sswhite.gitee.io/live2d_api/"   //存放在gitee上 可以fork到自己仓库
 		});
 	});
 }
